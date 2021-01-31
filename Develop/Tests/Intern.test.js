d8580f4a4530553e90800f8077537b12df176d38
EJostad <eric.jostad@outlook.com>
2021-01-30 19:47:54 -0800

    Renamed Dist folder to Output.  Added index.html file to Output folder.

--- Develop/Tests/Intern.test.js
+++ Develop/Tests/Intern.test.js
@@ -2,19 +2,19 @@
 const Intern = require("../lib/Intern");
 
 it("Shoud set school via constructor", () => {
-  const testValue = "UCF";
-  const emp = new Intern("Test", 1, "test@test.com", testValue);
-  expect(emp.school).toEqual(testValue);
+    const testValue = "UCF";
+    const emp = new Intern("Test", 1, "test@test.com", testValue);
+    expect(emp.school).toEqual(testValue);
 });
 
 it("Should getRole() & return \"Intern\"", () => {
-  const testValue = "Intern";
-  const emp = new Intern("Test", 1, "test@test.com", "UCF");
-  expect(emp.getRole()).toEqual(testValue);
+    const testValue = "Intern";
+    const emp = new Intern("Test", 1, "test@test.com", "UCF");
+    expect(emp.getRole()).toEqual(testValue);
 });
 
 it("Should get school via getSchool()", () => {
-  const testValue = "UCF";
-  const emp = new Intern("Test", 1, "test@test.com", testValue);
-  expect(emp.getSchool()).toEqual(testValue);
+    const testValue = "UCF";
+    const emp = new Intern("Test", 1, "test@test.com", testValue);
+    expect(emp.getSchool()).toEqual(testValue);
 });
